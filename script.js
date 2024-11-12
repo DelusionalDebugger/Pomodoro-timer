1242ea4809892521e868c2542f6436cfc6588ad5
DelusionalDebugger <rogid9607r@gmail.com>
2024-11-12 20:43:30 +0000

    Trying to set up a time and deadline system in the script.js file

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 import dayjs from dayjs
 console.log("This works")
+
+var study_timer = dayjs();
+var study_deadline = study_timer.minute(study_timer.minute() + 25)
+console.log(study_deadline)
+//Format('YYYY-MM-DD HH:mm:ss')
