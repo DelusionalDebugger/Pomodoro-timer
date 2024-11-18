4a7631c299d465b93b301d0a2dfebbacb5f127aa
DelusionalDebugger <rogid9607r@gmail.com>
2024-11-18 19:25:48 +0000

    Adding comments to the HTML file ad deletig unnecessary code from javascript file

--- script.js
+++ script.js
@@ -3,29 +3,3 @@ setTimeout(() => {
   //Runs the code here after the given time
   
 }, 1500000); //This is 25 * 60 * 1000 to get 25 minutes in milliseconds
-
-
-// Code below is from google chrome extension tutorial that gets the reading time of the extension
-
-const article = document.querySelector("article");
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
