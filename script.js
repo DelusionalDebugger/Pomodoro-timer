bcd96e207191510a12994a2b1801b315a179e3e8
DelusionalDebugger <rogid9607r@gmail.com>
2024-11-21 20:01:17 +0000

    Script ow works when putting it in the HTML file instead of the javascript file

--- script.js
+++
@@ -1,4 +0,0 @@
-//Using built in nodejs function to make a timer
-setTimeout(() => {
-  //Runs the code here after the given time
-}, 1500000); //This is 25 * 60 * 1000 to get 25 minutes in milliseconds
